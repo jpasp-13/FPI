27b5492d8ec92a1e930e88d2e122670bc1dc6c03
jpasp-13 <jpasp@cin.ufpe.br>
2026-03-08 17:03:08 -0300

    .

--- atvcode3.c
+++ atvcode3.c
@@ -22,13 +22,42 @@ int main(){
             printf("E parece que Rebeka vai ter que voltar andando...\n");
         }
 
-        
-    } else {
-        
-    
+     } else {
+        dinheiro += 1;
+        dinheiroRebeka -= 1;
+        valora = dinheiro * (a/100.0);
+        valorb = dinheiro * (b/100.0);
+        valorc = dinheiro * (c/100.0);
+        int sobra = dinheiro - (valora + valorb + valorc);
+        if(((dinheiro * a) % 100 == 0 && (dinheiro * b) % 100 == 0 && (dinheiro * c) % 100 == 0) && (sobra > 1)){
+            printf("Cada homem ficou com %d, %d e %d reais, respectivamente\n", (int)valora, (int)valorb, (int)valorc);
+            dinheiroRebeka += sobra;
+            if (dinheiroRebeka >= 7){
+                printf("Ela conseguiu! Rebeka voltou para casa e apanhou da mae por sumir noite passada!\n");
+            }
+            else{
+                printf("E parece que Rebeka vai ter que voltar andando...\n");
+            }            
+        }
+        else{
+            sobra = 0;
+            dinheiro += 1;
+            dinheiroRebeka -= 1;
+            valora = dinheiro * (a/100.0);
+            valorb = dinheiro * (b/100.0);
+            valorc = dinheiro * (c/100.0);
+            sobra = dinheiro - (valora + valorb + valorc);
+            if(((dinheiro * a) % 100 == 0 && (dinheiro * b) % 100 == 0 && (dinheiro * c) % 100 == 0) && (sobra > 2)){
+            printf("Cada homem ficou com %d, %d e %d reais, respectivamente\n", (int)valora, (int)valorb, (int)valorc);
+            dinheiroRebeka += sobra;
+            if (dinheiroRebeka >= 7){
+                printf("Ela conseguiu! Rebeka voltou para casa e apanhou da mae por sumir noite passada!\n");
+            }
+            else{
+                printf("E parece que Rebeka vai ter que voltar andando...\n");                
+            }
+        }
     }
-    
-    
-
-    return 0;
+}
+     return 0;
 }
