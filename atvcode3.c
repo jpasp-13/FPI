54b761c25f584b521809dca16ee677a5ef921e8c
jpasp-13 <jpasp@cin.ufpe.br>
2026-03-09 00:31:11 -0300

    .

--- atvcode3.c
+++ atvcode3.c
@@ -48,20 +48,50 @@ int main(){
             valorc = dinheiro * (c/100.0);
             sobra = dinheiro - (valora + valorb + valorc);
             if(((dinheiro * a) % 100 == 0 && (dinheiro * b) % 100 == 0 && (dinheiro * c) % 100 == 0) && (sobra > 2)){
-            printf("Cada homem ficou com %d, %d e %d reais, respectivamente\n", (int)valora, (int)valorb, (int)valorc);
-            dinheiroRebeka += sobra;
-            char X, Y, Z;
-            scanf(" %c %c %c", &X, &Y, &Z);
-            int soma = (X - 'a' + 1) + (Y - 'a' + 1) + (Z - 'a' + 1);
-            printf("%d\n", soma);
+                printf("Cada homem ficou com %d, %d e %d reais, respectivamente\n", (int)valora, (int)valorb, (int)valorc);
+                dinheiroRebeka += sobra;
+                char X, Y, Z;
+                scanf(" %c %c %c", &X, &Y, &Z);
+                int soma = (X - 'a' + 1) + (Y - 'a' + 1) + (Z - 'a' + 1);
+                printf("%d\n", soma);
 
-            if (dinheiroRebeka >= 7){
-                printf("Ela conseguiu! Rebeka voltou para casa e apanhou da mae por sumir noite passada!\n");
+                if (dinheiroRebeka >= 7){
+                    printf("Ela conseguiu! Rebeka voltou para casa e apanhou da mae por sumir noite passada!\n");
+                }
+                else{
+                    printf("E parece que Rebeka vai ter que voltar andando...\n");                
+                }
             }
             else{
-                printf("E parece que Rebeka vai ter que voltar andando...\n");                
-            }
-        }
+                sobra = 0;
+                dinheiro += 1;
+                dinheiroRebeka -= 1;
+                valora = dinheiro * (a/100.0);
+                valorb = dinheiro * (b/100.0);
+                valorc = dinheiro * (c/100.0);
+                sobra = dinheiro - (valora + valorb + valorc);
+                if(((dinheiro * a) % 100 == 0 && (dinheiro * b) % 100 == 0 && (dinheiro * c) % 100 == 0) && (sobra > 2)){
+                    printf("Cada homem ficou com %d, %d e %d reais, respectivamente\n", (int)valora, (int)valorb, (int)valorc);
+                    dinheiroRebeka += sobra;
+                    int id1, id2, id3;
+                    scanf(" %d %d %d", &id1, &id2, &id3);
+                    if (id1 % 3 == 0 || id2 % 3 == 0 || id3 % 3 == 0){
+                        id1 /= 3;
+                        id2 /= 3;
+                        id3 /= 3;
+                        int parcelas = (id1 + id2 + id3);
+                        printf("%d", parcelas);
+                    }
+                    if (dinheiroRebeka >= 7){
+                        printf("Ela conseguiu! Rebeka voltou para casa e apanhou da mae por sumir noite passada!\n");
+                    }
+                    else{
+                        printf("E parece que Rebeka vai ter que voltar andando...\n");                
+                    }
+                }
+                else{
+                    printf("Nao foi dessa vez que Rebeka pode ajudar...\n");
+                }
     }
 }
      return 0;
