06814a8284bfd811bf869ed0e5e2f7fcf0255de6
jpasp-13 <jpasp@cin.ufpe.br>
2026-03-31 14:18:00 -0300

    .

--- Atvcode1.c
+++ Atvcode1.c
@@ -1,34 +1,58 @@
-#include<stdio.h>
-int main(){
-    char letra;
-    scanf("%c",&letra);
-    for(char i = 'A'; i <= letra; i++){
-         char l = i;
-        while(l - letra < 0){
-            printf(".");
-            l++;
-        }
-        if(i > 'A' && i <= letra){
-            for(char v = 'A'; v < i; v++){
-                printf("%c", v);
-            }
-        }
+#include <stdio.h>
 
+int main() {
+    int M, N;
+    scanf("%d %d", &M, &N);
 
-        
-        if(i >= 'A' && i <= letra){
-            for(char v = i; v >= 'A'; --v){
-                printf("%c", v);
+    char grid[M][N+1];
+    for (int i = 0; i < M; i++)
+        scanf("%s", grid[i]);
+
+    for (int sr = 0; sr < M; sr++) {
+        for (int sc = 0; sc < N; sc++) {
+            if (grid[sr][sc] != 'O')
+                goto next_cell;
+
+            {
+                int r = sr, c = sc;
+                while (r + 1 < M && c >= 0 && c < N) {
+                    if (grid[r+1][c] == '#') {
+                        int left = c, right = c;
+                        while (left - 1 >= 0 && grid[r+1][left-1] == '#')
+                            left--;
+                        while (right + 1 < N && grid[r+1][right+1] == '#')
+                            right++;
+
+                        if (c - left <= right - c) {
+                            int cc = c;
+                            while (cc >= left) {
+                                if (grid[r][cc] == '.')
+                                    grid[r][cc] = 'O';
+                                cc--;
+                            }
+                            c = left - 1;
+                        } else {
+                            int cc = c;
+                            while (cc <= right) {
+                                if (grid[r][cc] == '.')
+                                    grid[r][cc] = 'O';
+                                cc++;
+                            }
+                            c = right + 1;
+                        }
+                    } else {
+                        grid[r+1][c] = 'O';
+                        r++;
+                    }
+                }
             }
-        }       
-        char c = i;
-        while(c - letra < 0){
-            printf(".");
-            c++;
+
+            next_cell:;
         }
-        printf("\n");
-    
     }
-    
+
+    for (int i = 0; i < M; i++)
+        printf("%s\n", grid[i]);
+
     return 0;
 }
